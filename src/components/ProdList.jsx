16513654fd551747269a4f04a83f8fdce264a06e
Florenz Elvin Manaois <z5366991@ad.unsw.edu.au>
2024-09-27 10:46:27 +1000

    Hotfix UI

--- src/components/ProdList.jsx
+++ src/components/ProdList.jsx
@@ -1,59 +1,60 @@
-import React, { useState } from 'react'
-import ProductWidget from './ProductWidget'
-import Loading from './Loading';
+import React, { useState } from "react";
+import ProductWidget from "./ProductWidget";
+import Loading from "./Loading";
 
-const ProdList = ({ title = '', products, isLoading = false }) => {
+const ProdList = ({ title = "", products, isLoading = false }) => {
+  const [sortOption, setSortOption] = useState("default");
 
-  const [isSorted, setIsSorted] = useState(false);
-
-  const toggleSort = () => setIsSorted(prev => !prev);
-
-  const sortedProdList = [...products].sort((a, b) => {
-    if (isSorted) {
-      return a.node.title.localeCompare(b.node.title);
+  const sortProducts = () => {
+    if (sortOption === "name") {
+      return [...products].sort((a, b) =>
+        a.node.title.localeCompare(b.node.title)
+      );
+    } else if (sortOption === "price") {
+      return [...products].sort(
+        (a, b) =>
+          parseFloat(a.node.priceRange.minVariantPrice.amount) -
+          parseFloat(b.node.priceRange.minVariantPrice.amount)
+      );
+    } else {
+      return products;
     }
-    return 0;
-  });
+  };
+
+  // Apply sorting function
+  const sortedProducts = sortProducts();
 
   return (
     <div className="bg-white">
       <div className="max-w-2xl mx-auto py-10 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className='flex items-center justify-between'>
+        <div className="flex items-center justify-between">
           <h2 className="text-2xl font-extrabold text-gray-900 mb-6">
             {title}
           </h2>
-          <button
-            onClick={toggleSort}
-            className="hidden sm:block mb-4 bg-transparent text-black hover:bg-gray-100 px-4 py-2 rounded"
+         <select
+            className="p-0 appearance-none sm:appearance-auto sm:p-2"
+            value={sortOption}
+            onChange={(e) => setSortOption(e.target.value)}
           >
-            {isSorted ? "Remove Alphabetical Sort" : "Sort Alphabetically"}
-          </button>
-
-          <button
-            onClick={toggleSort}
-            className="sm:hidden mb-4 bg-transparent text-black hover:bg-gray-100 px-4 py-2 rounded"
-          >
-            {isSorted ? "Unsort" : "Sort"}
-          </button>
+            <option value="default" hidden>Sort Items</option>
+            <option value="name">Sort by Name</option>
+            <option value="price">Sort by Price</option>
+          </select>
         </div>
-        {
-          isLoading
-            ?
-            <div className='w-full flex justify-center'>
-              <Loading />
-            </div>
-            :
-            <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-              {
-                sortedProdList.map(product => (
-                  <ProductWidget key={product.node.id} product={product} />
-                ))
-              }
-            </div>
-        }
+        {isLoading ? (
+          <div className="w-full flex justify-center">
+            <Loading />
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {sortedProducts.map((product) => (
+              <ProductWidget key={product.node.id} product={product} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProdList
+export default ProdList;
